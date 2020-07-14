ac6382d18789fda54fdf628a36b3d4d585a3f792
Jonathan-Lynch <jonathanlynch2001@gmail.com>
2020-07-14 11:55:22 -0500

    Finished Models

--- JonathanGraduationBookApp.Core/Model/BookModel.cs
+++ JonathanGraduationBookApp.Core/Model/BookModel.cs
@@ -10,9 +10,11 @@ namespace JonathanGraduationBookApp.Core.Model
 		public string Title { get; set; }
 		public string Genre { get; set; }
 
+		//connects too AuthorModel
 		public int AuthorId { get; set; }
 		public AuthorModel Author { get; set; }
 
+		//connects too PublisherModel
 		public int PublisherId { get; set; }
 		public PublisherModel Publisher { get; set; }
 	}
